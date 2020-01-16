ab0ad2b4d6b71ef85318e43e8da223b8a29c52b8
Arsh Sekhon <arshsekhon1998@gmail.com>
2020-01-15 20:33:42 -0800

    Fixed entrypoint shell scripts

--- jenkins/jenkins_setup.sh
+++ jenkins/jenkins_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# This file was derived from: https://github.com/linagora/docker-sonarqube-pr/blob/master/start_with_profile.sh
 
 function curlAdmin {
     curl -u $SONAR_USER_LOGIN:$SONAR_USER_PASSWORD $@
--- jenkins/sonarqube/setup.sh
+++ jenkins/sonarqube/setup.sh
@@ -1,7 +1,6 @@
-# This file was derived from: 
-
 #!/bin/bash
 #
+# This file was derived from: https://github.com/linagora/docker-sonarqube-pr/blob/master/start_with_profile.sh
 
 # Start Sonar
 ./bin/run.sh &
