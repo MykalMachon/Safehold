f02bd17845996102fab8c3ba23874669d7ed4e94
Arsh Sekhon <arshsekhon1998@gmail.com>
2020-03-15 00:33:36 -0700

    Added preferences to the app.

--- android/app/src/main/java/com/tinybox/safehold/ui/account/Account.java
+++ android/app/src/main/java/com/tinybox/safehold/ui/account/Account.java
@@ -1,36 +1,25 @@
 package com.tinybox.safehold.ui.account;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.tinybox.safehold.R;
 
-public class Account extends Fragment {
+public class Account extends PreferenceFragmentCompat {
 
     private AccountViewModel accountViewModel;
 
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        accountViewModel =
-                ViewModelProviders.of(this).get(AccountViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
