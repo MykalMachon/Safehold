6bc917dc11b764733fdf4f95b9f223fcceeecf69
Arsh Sekhon <arshsekhon1998@gmail.com>
2020-03-17 20:20:20 -0700

    Did Styling and added Emergency Contacts Activity

--- android/app/src/main/java/com/tinybox/safehold/ui/account/Account.java
+++ android/app/src/main/java/com/tinybox/safehold/ui/account/Account.java
@@ -1,14 +1,17 @@
 package com.tinybox.safehold.ui.account;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.tinybox.safehold.R;
@@ -17,9 +20,9 @@ public class Account extends PreferenceFragmentCompat {
 
     private AccountViewModel accountViewModel;
 
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
+        addPreferencesFromResource(R.xml.preferences);
+
     }
 }
--- android/app/src/main/java/com/tinybox/safehold/ui/home/HomeFragment.java
+++ android/app/src/main/java/com/tinybox/safehold/ui/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -24,13 +25,8 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         return root;
     }
 }
