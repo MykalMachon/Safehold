662c18b1f58a2216a47b3114dc9b424d0239436d
Mykal Machon <mykalmachon@gmail.com>
2020-03-22 22:09:59 -0700

    Further Designing Survey Activity

--- android/app/src/main/java/com/tinybox/safehold/ui/survey/SurveyActivity.java
+++ android/app/src/main/java/com/tinybox/safehold/ui/survey/SurveyActivity.java
@@ -1,8 +1,11 @@
 package com.tinybox.safehold.ui.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.tinybox.safehold.R;
 
@@ -12,5 +15,26 @@ public class SurveyActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_survey);
+
+    // Set Toolbar at the top of the activity
+    Toolbar toolbar = findViewById(R.id.survey_toolbar);
+    toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp,null));
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("Survey");
+
+    // Set the spinner for question 2
+    Spinner qTwoSpinner = (Spinner) findViewById(R.id.Survey_AnswerTwo);
+    ArrayAdapter<CharSequence> qTwoAdapter = ArrayAdapter.createFromResource(this, R.array.survey_question_two_options, android.R.layout.simple_spinner_dropdown_item);
+    qTwoSpinner.setAdapter(qTwoAdapter);
+
+    // Set the spinner for question 3
+    Spinner qThreeSpinner = (Spinner) findViewById(R.id.Survey_AnswerThree);
+    ArrayAdapter<CharSequence> qThreeAdapter = ArrayAdapter.createFromResource(this, R.array.survey_question_three_options, android.R.layout.simple_spinner_dropdown_item);
+    qThreeSpinner.setAdapter(qThreeAdapter);
+  }
+
+  public boolean onSupportNavigateUp() {
+    onBackPressed();
+    return true;
   }
 }
