c985dc941e1a07ab0a2cd932b19540cd51f9115a
Hieu Le <hieu.le@student.ufv.ca>
2020-04-08 16:40:17 -0700

    Added checking location permission in Survey class

--- android/app/src/main/java/com/tinybox/safehold/ui/survey/SurveyActivity.java
+++ android/app/src/main/java/com/tinybox/safehold/ui/survey/SurveyActivity.java
@@ -1,6 +1,7 @@
 package com.tinybox.safehold.ui.survey;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -12,8 +13,11 @@ import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.tinybox.safehold.R;
+import com.tinybox.safehold.ui.map.PermissionUtils;
 import com.tinybox.safehold.utility.FormActivity;
 
 import org.json.JSONException;
@@ -22,6 +26,10 @@ import org.json.JSONObject;
 import java.util.Date;
 
 public class SurveyActivity extends FormActivity {
+  private static final int LOCATION_PERMISSION_REQUEST_CODE = 11;
+  double longitude;
+  double latitude;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,7 @@ public class SurveyActivity extends FormActivity {
 
     // Set Toolbar at the top of the activity
     Toolbar toolbar = findViewById(R.id.survey_toolbar);
-    toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp,null));
+    toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp, null));
     setSupportActionBar(toolbar);
     getSupportActionBar().setTitle("Mind Sharing What Happened?");
 
@@ -59,14 +67,20 @@ public class SurveyActivity extends FormActivity {
   private JSONObject createJsonObject() {
     JSONObject rootJson = new JSONObject();
     JSONObject requestJson = new JSONObject();
-    try{
+    try {
       MultiAutoCompleteTextView questionOne = findViewById(R.id.Survey_AnswerOne);
       Spinner questionTwo = findViewById(R.id.Survey_AnswerTwo);
       Spinner questionThree = findViewById(R.id.Survey_AnswerThree);
-      LocationManager lm = (LocationManager)getSystemService(LOCATION_SERVICE);
+      LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+      // Checking GPS permission
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        PermissionUtils.requestPermission((FragmentActivity) getApplicationContext(), LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION, true);
+      }
+
       Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-      double longitude = location.getLongitude();
-      double latitude = location.getLatitude();
+      longitude = location.getLongitude();
+      latitude = location.getLatitude();
       requestJson.put("createTime", new Date().toString());
       requestJson.put("reasonForEvent", questionOne.getText());
       requestJson.put("areYouSafe", questionTwo.getSelectedItem().toString());
