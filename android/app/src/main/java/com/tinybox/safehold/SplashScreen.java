babcce56415fae1c02832b05689e85f8c7f60a50
Hieu Le <hieu.le@student.ufv.ca>
2020-04-08 21:46:19 -0700

    Revised SplashScreen class

--- android/app/src/main/java/com/tinybox/safehold/SplashScreen.java
+++ android/app/src/main/java/com/tinybox/safehold/SplashScreen.java
@@ -7,21 +7,29 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
 
+        runnable = new Runnable() {
             @Override
             public void run() {
-
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
-
-                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
-        }, 500);
+        };
+
+        handler = new Handler();
+        handler.postDelayed(runnable, 500);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handler != null && runnable != null)
+            handler.removeCallbacks(runnable);
     }
 }
